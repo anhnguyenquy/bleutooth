c116e4703a648911074d821628144628135123ea
Anh Nguyen <anhnguyenquy2407@gmail.com>
2021-12-23 04:28:25 +0700

    Removed outdated files

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -1,12 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.subsystems.Intakers;
 import frc.robot.subsystems.Drivebase;
 
 public class Auto extends ParallelCommandGroup {
   
-  public Auto(Intakers intakers, Drivebase drive ) {
+  public Auto(Drivebase drive ) {
     addCommands(
 
     );
